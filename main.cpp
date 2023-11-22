1fd2f9de4478bda9b5a1cc3eaf17935aa9cc7c05
Cristian Felipe <estilo-callejero@hotmail.de>
2023-11-22 15:51:42 +0100

    1.0

--- main.cpp
+++ main.cpp
@@ -5,8 +5,6 @@
 
 int main()
 {
-	//Number n("0x14");
-    //Converter converter(&n);
 	std::string userInput;
 	while (true)
 	{
@@ -20,6 +18,7 @@ int main()
 		std::cout << std::endl;
 		std::cout << std::endl;
 		std::cout << std::endl;
+
 		if (userInput == "exit")
 		{
 			return 0;
@@ -27,7 +26,6 @@ int main()
 
 		Number num(userInput);
 		Converter converter(&num);
-
 	}
 
 	return 0;
